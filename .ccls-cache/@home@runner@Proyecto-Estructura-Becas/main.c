0f2b9396bce2fab085a986960f1090d8688abd51
Vicente <125922459+viichorm@users.noreply.github.com>
2024-06-28 10:51:19 +0000

    Commit Prueba

--- .ccls-cache/@home@runner@Proyecto-Estructura-Becas/main.c
+++ .ccls-cache/@home@runner@Proyecto-Estructura-Becas/main.c
@@ -63,7 +63,7 @@ void limpiarPantalla() {
 
 //Función presione tecla para Continuar
 void presioneTeclaParaContinuar() {
-  puts("Presione una tecla para continuar...");
+  puts("Presione una tecla para continuar......");
   getchar(); // Consume el '\n' del buffer de entrada
   getchar(); // Espera a que el usuario presione una tecla
 }
